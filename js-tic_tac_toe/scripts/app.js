b6bc626439eb22506f6e82745d1a86701dbb3bb1
pfalz <mega75581@gmail.com>
2023-04-21 19:52:44 +0900

    added savePlyaerConfig function to config.js file

--- js-tic_tac_toe/scripts/app.js
+++ js-tic_tac_toe/scripts/app.js
@@ -1,11 +1,27 @@
+let editedPlayer = 0;
+const players = [
+  {
+    name: "Player 1",
+    symbol: "X",
+  },
+  {
+    name: "Player 2",
+    symbol: "O",
+  },
+];
+
 const playerConfigOverlay = document.querySelector("#config-overlay");
 const backdrop = document.querySelector("#backdrop");
 const editPlayer1Btn = document.querySelector("#edit-player1");
 const editPlayer2Btn = document.querySelector("#edit-player2");
 const cancelConfig = document.querySelector("#cancel-config");
+const formElement = document.querySelector("form");
+const errorsOutput = document.querySelector("#config-errors");
 
 editPlayer1Btn.addEventListener("click", openPlayerConfig);
 editPlayer2Btn.addEventListener("click", openPlayerConfig);
 
 cancelConfig.addEventListener("click", closePlayerConfig);
 backdrop.addEventListener("click", closePlayerConfig);
+
+formElement.addEventListener("submit", savePlayerConfig);
--- js-tic_tac_toe/scripts/config.js
+++ js-tic_tac_toe/scripts/config.js
@@ -1,4 +1,5 @@
-function openPlayerConfig() {
+function openPlayerConfig(event) {
+  editedPlayer = Number(event.target.dataset.playerid);
   playerConfigOverlay.style.display = "block";
   backdrop.style.display = "block";
 }
@@ -6,4 +7,30 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   playerConfigOverlay.style.display = "none";
   backdrop.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutput.innerHTML = "";
+
+  const inputData = document.querySelector("#playername");
+  inputData.value = "";
+}
+
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayerName = formData.get("playername").trim();
+
+  if (!enteredPlayerName) {
+    event.target.firstElementChild.classList.add("error");
+    errorsOutput.innerHTML = "Please enter a valid name.";
+    return;
+  }
+
+  const updatedPlayerData = document.querySelector(
+    `#player${editedPlayer}-data`
+  );
+  updatedPlayerData.children[1].innerHTML = enteredPlayerName;
+
+  players[editedPlayer - 1].name = enteredPlayerName;
+
+  closePlayerConfig();
 }
