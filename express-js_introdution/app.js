5f3ea9e095fb6b34bc5389445156e0c97cfeedbd
pfalz <mega75581@gmail.com>
2023-05-05 16:47:46 +0900

    modified express-js_introduction project

--- express-js_introdution/app.js
+++ express-js_introdution/app.js
@@ -1,15 +1,52 @@
-const http = require(`http`);
+const fs = require(`fs`);
 
-function handleRequest(request, response) {
-  if (request.url === `/currenttime`) {
-    response.statusCode = 200;
-    response.end(`<h1>${new Date().toISOString()}</h1>`);
-  } else if (request.url === `/`) {
-    response.statusCode = 200;
-    response.end(`<h1>Hello world!</h1>`);
+const path = require(`path`);
+
+const express = require(`express`);
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get(`/`, function (req, res) {
+  res.send(
+    `<form action="/store-user" method="POST"><label>Your name</label><input type="text" name="username"><button>Submit</button></form>`
+  );
+});
+
+app.post(`/store-user`, function (req, res) {
+  const userName = req.body.username;
+
+  const filePath = path.join(__dirname, `data`, `users.json`);
+
+  const fileData = fs.readFileSync(filePath);
+  const existingUsers = JSON.parse(fileData);
+
+  existingUsers.push(userName);
+  fs.writeFileSync(filePath, JSON.stringify(existingUsers));
+
+  res.send(`<h1>Username stored.</h1>`);
+});
+
+app.get(`/currenttime`, function (req, res) {
+  res.send(`<h1>${new Date().toISOString()}</h1>`);
+});
+
+app.get(`/users`, function(req, res){
+  const filePath = path.join(__dirname, `data`, `users.json`);
+
+  const fileData = fs.readFileSync(filePath);
+  const existingUsers = JSON.parse(fileData);
+
+  let resData = `<ul>`;
+
+  for(const user of existingUsers){
+    resData += `<li>${user}</li>`;
   }
-}
 
-const server = http.createServer(handleRequest);
+  resData += `</ul>`;
 
-server.listen(3000);
+  res.send(resData);
+})
+
+app.listen(3000);
