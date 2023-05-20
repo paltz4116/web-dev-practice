c3978355fe4c3638a7a431f2a205bf2ad3ab94d7
pfalz <mega75581@gmail.com>
2023-05-20 17:25:19 +0900

    added 404 error page

--- express-handling-dynamic-routes/app.js
+++ express-handling-dynamic-routes/app.js
@@ -40,6 +40,8 @@ app.get("/restaurants/:id", function (req, res) {
       return res.render(`restaurant-detail`, { restaurant: restaurant });
     }
   }
+
+  res.render(`404`);
 });
 
 app.get("/recommend", function (req, res) {
@@ -67,6 +69,10 @@ app.get("/confirm", function (req, res) {
 
 app.get("/about", function (req, res) {
   res.render("about");
+});
+
+app.use(function(req, res) {
+  res.render(`404`);
 });
 
 app.listen(3000);
