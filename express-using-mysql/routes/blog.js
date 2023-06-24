d195b33ce27eb347591f2ea9a749f7e7fb9c6ba7
pfalz <mega75581@gmail.com>
2023-06-24 15:11:38 +0900

    preparing update post page

--- express-using-mysql/routes/blog.js
+++ express-using-mysql/routes/blog.js
@@ -22,15 +22,34 @@ router.get(`/new-post`, async function (req, res) {
   res.render(`create-post`, { authors: authors });
 });
 
-router.post(`/posts`, async function(req, res){
+router.post(`/posts`, async function (req, res) {
   const data = [
     req.body.title,
     req.body.summary,
     req.body.content,
-    req.body.author
+    req.body.author,
   ];
-  await db.query(`INSERT INTO posts (title, summary, body, author_id) VALUES (?)`, [data]);
+  await db.query(
+    `INSERT INTO posts (title, summary, body, author_id) VALUES (?)`,
+    [data]
+  );
   res.redirect(`/posts`);
+});
+
+router.get(`/posts/:id`, async function (req, res) {
+  const query = `
+    SELECT posts.*, authors.name AS author_name, authors.email AS author_email FROM posts 
+    INNER JOIN authors ON posts.author_id = authors.id
+    WHERE posts.id = ?
+  `;
+
+  const [posts] = await db.query(query, [req.params.id]);
+
+  if(!posts != posts.length === 0){
+    return res.status(404).render(`404`);
+  }
+
+  res.render(`post-detail`, { post: posts[0]} );
 });
 
 module.exports = router;
