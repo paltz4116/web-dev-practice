ded07699eb8aedf94bd8505dfd168f4918556ca5
pfalz <mega75581@gmail.com>
2023-07-04 15:07:46 +0900

    updating the dom

--- ajax-aync-js-http/public/scripts/comments.js
+++ ajax-aync-js-http/public/scripts/comments.js
@@ -1,11 +1,32 @@
 const loadCommentsBtn = document.querySelector(`#load-comments`);
+const commentsSection = document.querySelector(`#comments`);
 
-async function fetchCommentsForPost(event) {
-    const postId = loadCommentsBtn.dataset.postid;
-    const response = await fetch(`/posts/${postId}/comments`);
-    const responseData = await response.json();
+function createCommentsList(comments) {
+  const commentList = document.createElement(`ol`);
 
-    console.log(responseData);
+  for (const comment of comments) {
+    const commentElement = document.createElement(`li`);
+    commentElement.innerHTML = `
+    <article class="comment-item">
+        <h2>${comment.title}</h2>
+        <p>${comment.text}</p>
+    </article>
+  `;
+
+  commentList.appendChild(commentElement);
+  }
+
+  return commentList;
+}
+
+async function fetchCommentsForPost() {
+  const postId = loadCommentsBtn.dataset.postid;
+  const response = await fetch(`/posts/${postId}/comments`);
+  const responseData = await response.json();
+
+  const commentsList = createCommentsList(responseData);
+  commentsSection.innerHTML = ``;
+  commentsSection.appendChild(commentsList);
 }
 
 loadCommentsBtn.addEventListener(`click`, fetchCommentsForPost);
