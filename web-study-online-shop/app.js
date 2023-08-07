ae5d468798a3042c618840500864d665ead98dd7
pfalz <mega75581@gmail.com>
2023-08-07 17:44:05 +0900

    added error handling middleware and session configuration

--- web-study-online-shop/app.js
+++ web-study-online-shop/app.js
@@ -1,11 +1,13 @@
 const path = require(`path`);
 
 const express = require(`express`);
-
 const csrf = require(`csurf`);
+const expressSession = require(`express-session`);
 
+const createSessionConfig = require(`./config/session`);
 const db = require(`./data/database`);
 const addCsrfTokenMiddleware = require(`./middleware/csrf-token`);
+const errodHandlingMiddleware = require(`./middleware/error-handler`);
 const authRoutes = require(`./routes/auth-routes`);
 
 const app = express();
@@ -16,11 +18,16 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
+const sessionConfig = createSessionConfig();
+
+app.use(expressSession(sessionConfig));
 app.use(csrf());
 
 app.use(addCsrfTokenMiddleware);
 
 app.use(authRoutes);
+
+app.use(errodHandlingMiddleware);
 
 db.connectToDatabase()
   .then(function () {
