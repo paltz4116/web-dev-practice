0c0d4d508e1a5c52cdcc39c231b904afc02d7ffd
pfalz <mega75581@gmail.com>
2023-08-07 16:04:52 +0900

    added signup function

--- web-study-online-shop/data/database.js
+++ web-study-online-shop/data/database.js
@@ -1,5 +1,4 @@
 const mongodb = require(`mongodb`);
-const { get } = require("../routes/auth-routes");
 
 const MongoClient = mongodb.MongoClient;
 
@@ -7,7 +6,7 @@ let database;
 
 async function connectToDatabase() {
   const client = await MongoClient.connect(`mongodb://localhost:27017`);
-  database = client.db(`onlien-shop`);
+  database = client.db(`online-shop`);
 }
 
 function getDb() {
