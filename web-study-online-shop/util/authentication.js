f689bfbed049e809a42e7c23ad09c1d256411804
pfalz <mega75581@gmail.com>
2023-08-09 15:19:10 +0900

    added admin authorization and nav element css style

--- web-study-online-shop/util/authentication.js
+++ web-study-online-shop/util/authentication.js
@@ -1,5 +1,6 @@
 function createUserSession(req, user, action) {
   req.session.uid = user._id.toString();
+  req.session.isAdmin = user.isAdmin;
   req.session.save(action); //세션이 저장되기 전에 인증된 페이지에 접근하는 것을 방지
 }
 
