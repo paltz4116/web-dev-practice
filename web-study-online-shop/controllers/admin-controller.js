db03d13656863640377b02e63237072e62d85f66
pfalz <mega75581@gmail.com>
2023-08-10 23:26:17 +0900

    connected products to mongodb database

--- web-study-online-shop/controllers/admin-controller.js
+++ web-study-online-shop/controllers/admin-controller.js
@@ -1,14 +1,27 @@
+const Product = require(`../models/product-model`);
+
 function getProducts(req, res) {
   res.render(`admin/products/all-products`);
 }
 
 function getNewProduct(req, res) {
-    res.render(`admin/products/new-product`);
+  res.render(`admin/products/new-product`);
 }
 
-function createNewProduct(req, res) {
-  console.log(req.body);
-  console.log(req.file);
+async function createNewProduct(req, res, next) {
+  const product = new Product({
+    ...req.body,
+    image: req.file.filename,
+  });
+
+  try {
+    await product.save();
+  } catch (error) {
+    next(error);
+    return;
+  }
+
+  res.redirect(`/admin/products`);
 }
 
 module.exports = {
