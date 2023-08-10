fed2fd766748181e134c57bb2c406eec6c4f8dd4
pfalz <mega75581@gmail.com>
2023-08-10 22:46:59 +0900

    added file uploading function

--- web-study-online-shop/routes/admin-routes.js
+++ web-study-online-shop/routes/admin-routes.js
@@ -1,11 +1,14 @@
 const express = require(`express`);
 
 const adminController = require(`../controllers/admin-controller`);
+const imageUploadMiddleware = require(`../middleware/image-uploads`);
 
 const router = express.Router();
 
 router.get(`/products`, adminController.getProducts);
 
 router.get(`/products/new`, adminController.getNewProduct);
+
+router.post(`/products`, imageUploadMiddleware, adminController.createNewProduct)
 
 module.exports = router;
