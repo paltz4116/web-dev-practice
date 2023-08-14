dc0709726a53d5ab46dbbfbaa7daaf539df1e481
pfalz <mega75581@gmail.com>
2023-08-14 18:44:11 +0900

    outputting product information

--- web-study-online-shop/routes/products-routes.js
+++ web-study-online-shop/routes/products-routes.js
@@ -1,9 +1,10 @@
 const express = require(`express`);
+const productsController = require(`../controllers/products-controller`);
 
 const router = express.Router();
 
-router.get(`/products`, function(req, res){
-    res.render(`customer/products/all-products`);
-});
+router.get(`/products`, productsController.getAllProduct);
+
+router.get(`/products/:id`, productsController.getProduct);
 
 module.exports = router;
