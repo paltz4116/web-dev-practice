02e3a766a4bfe0dff916b3e39508bf3da94e15c6
pfalz <mega75581@gmail.com>
2023-08-23 17:33:31 +0900

    added stripe payment api code

--- web-study-online-shop/controllers/orders-controllers.js
+++ web-study-online-shop/controllers/orders-controllers.js
@@ -1,10 +1,14 @@
-const Order = require('../models/order-model');
-const User = require('../models/user-model');
+const stripe = require("stripe")(
+  "sk_test_51NiAtCBmU1RSub0uhhbXBLSou1DBmN19Bx8uSRSTZhdEAQVJ8yjkNjEcR7CeOb2DLj8sGZORImTinb5byuHZARmZ00Juv6TQhp"
+);
+
+const Order = require("../models/order-model");
+const User = require("../models/user-model");
 
 async function getOrders(req, res) {
   try {
     const orders = await Order.findAllForUser(res.locals.uid);
-    res.render('customer/orders/all-orders', {
+    res.render("customer/orders/all-orders", {
       orders: orders,
     });
   } catch (error) {
@@ -33,10 +37,39 @@ async function addOrder(req, res, next) {
 
   req.session.cart = null;
 
-  res.redirect('/orders');
+  const session = await stripe.checkout.sessions.create({
+    line_items: cart.items.map(function(item) {
+      return {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price_data: {
+          currency: `usd`,
+          product_data: {
+            name: `item.product.title`,
+          },
+          unit_amount: +item.product.price.toFixed(2) * 100,
+        },
+        quantity: item.quantity,
+      };
+    }),
+    mode: "payment",
+    success_url: `http://localhost:3000/orders/success`,
+    cancel_url: `http://localhost:3000/orders/failure`,
+  });
+
+  res.redirect(303, session.url);
+}
+
+function getSuccess(req, res) {
+  res.render(`customer/orders/success`);
+}
+
+function getFailure(req, res) {
+  res.render(`customer/orders/failure`);
 }
 
 module.exports = {
   addOrder: addOrder,
   getOrders: getOrders,
+  getSuccess: getSuccess,
+  getFailure: getFailure,
 };
