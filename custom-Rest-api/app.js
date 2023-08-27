bd3ae3b83ed1f0ed47402a83bfb9bcedf017de58
pfalz <mega75581@gmail.com>
2023-08-27 21:15:39 +0900

    enhancing the api

--- custom-Rest-api/app.js
+++ custom-Rest-api/app.js
@@ -1,11 +1,23 @@
 const express = require(`express`);
 
+const db = require(`./data/database`);
+
+const quoteRoutes = require(`./routes/quotes-route`);
+
 const app = express();
 
-app.get(`/quote`, function (req, res, next) {
-  res.json({
-    quote: `Testing rest api`,
+app.use(`/quote`, quoteRoutes);
+
+app.use(function (error, req, res, next) {
+  res.status(500).json({
+    message: `Something went wrong.`,
   });
 });
 
-app.listen(3000);
+db.initDb()
+  .then(function () {
+    app.listen(3000);
+  })
+  .catch(function (error) {
+    console.log(`Connecting to the database failed.`);
+  });
